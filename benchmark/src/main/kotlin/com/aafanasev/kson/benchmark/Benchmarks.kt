cb4d80001a67fb78cf7df5abd4e3d57f3b36d0e7
jokuskay <tafanasyev@gmail.com>
2018-05-26 00:16:28 +0700

    add benchmark module

---
+++ benchmark/src/main/kotlin/com/aafanasev/kson/benchmark/Benchmarks.kt
@@ -0,0 +1,21 @@
+package com.aafanasev.kson.benchmark
+
+import org.openjdk.jmh.annotations.Benchmark
+
+fun main(args: Array<String>) {
+    org.openjdk.jmh.Main.main(args)
+}
+
+open class Benchmarks {
+
+    @Benchmark
+    fun reflectiveTypeAdapter(gsonState: GsonState, jsonState: JsonState) {
+        val entity = gsonState.gson.fromJson(jsonState.json, Entity::class.java)
+    }
+
+    @Benchmark
+    fun generatedTypeAdapter(gsonState: GsonWithKsonState, jsonState: JsonState) {
+        val entity = gsonState.gson.fromJson(jsonState.json, Entity::class.java)
+    }
+
+}
---
+++ benchmark/src/main/kotlin/com/aafanasev/kson/benchmark/Entities.kt
@@ -0,0 +1,6 @@
+package com.aafanasev.kson.benchmark
+
+import com.aafanasev.kson.annotation.Kson
+
+@Kson
+data class Entity(val id: Int)
