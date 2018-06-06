53989f304596bdae23ab431cffcf00bd991cd98f
jokuskay <tafanasyev@gmail.com>
2018-06-07 00:08:29 +0700

    add type adapter name generator

--- kson-processor/src/main/kotlin/com/aafanasev/kson/processor/KProperty.kt
+++ kson-processor/src/main/kotlin/com/aafanasev/kson/processor/KProperty.kt
@@ -1,12 +1,43 @@
 package com.aafanasev.kson.processor
 
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.ParameterizedTypeName
 import com.squareup.kotlinpoet.TypeName
 
-internal data class KProperty(val key: String,
-                              val name: String,
-                              val type: TypeName,
-                              val nullable: Boolean) {
+internal data class KProperty(
+        val key: String,
+        val name: String,
+        val type: TypeName,
+        val nullable: Boolean
+) {
+    val adapterName = typeToAdapterName(type)
+}
 
-    val adapterName = "${name}TypeAdapter"
+internal fun typeToAdapterName(type: TypeName): String {
+    val names = mutableListOf<String>()
 
+    if (type is ParameterizedTypeName) {
+        names.addAll(type.getClassNamesRecursively())
+    } else {
+        names.add((type as ClassName).simpleName())
+    }
+
+    names.add("adapter")
+
+    return names.joinToString(separator = "_").toLowerCase()
+}
+
+private fun ParameterizedTypeName.getClassNamesRecursively(): List<String> {
+    val names = mutableListOf<String>()
+    names.add(rawType.simpleName())
+
+    typeArguments.forEach {
+        if (it is ParameterizedTypeName) {
+            names.addAll(it.getClassNamesRecursively())
+        } else {
+            names.add((it as ClassName).simpleName())
+        }
+    }
+
+    return names
 }
