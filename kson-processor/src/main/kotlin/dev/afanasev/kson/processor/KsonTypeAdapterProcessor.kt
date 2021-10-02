332349e76316514283502e7b1c0c489b3465ed2f
aafanasev <tafanasyev@gmail.com>
2021-10-02 21:57:30 +0100

    update package & version 2

--- kson-processor/src/main/kotlin/dev/afanasev/kson/processor/KsonTypeAdapterProcessor.kt
+++ kson-processor/src/main/kotlin/dev/afanasev/kson/processor/KsonTypeAdapterProcessor.kt
@@ -59,69 +59,69 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
         log("generating a type adapter for ${clazz.simpleName}...")
 
         val properties = clazz.enclosedElements
-                .asSequence()
-                .filter { it.kind == ElementKind.FIELD && !it.modifiers.contains(Modifier.STATIC) }
-                .map { it as VariableElement }
-                .map {
-                    val serializedNameAnnotation = it.getAnnotation(SerializedName::class.java)
-                    val key = serializedNameAnnotation?.value ?: it.simpleName
-                    val alternateKeys = serializedNameAnnotation?.alternate?.toSet() ?: emptySet()
-                    val name = it.simpleName
-                    val type = it.asType().asTypeName()
-                    val nullable = it.getAnnotation(Nullable::class.java) != null
+            .asSequence()
+            .filter { it.kind == ElementKind.FIELD && !it.modifiers.contains(Modifier.STATIC) }
+            .map { it as VariableElement }
+            .map {
+                val serializedNameAnnotation = it.getAnnotation(SerializedName::class.java)
+                val key = serializedNameAnnotation?.value ?: it.simpleName
+                val alternateKeys = serializedNameAnnotation?.alternate?.toSet() ?: emptySet()
+                val name = it.simpleName
+                val type = it.asType().asTypeName()
+                val nullable = it.getAnnotation(Nullable::class.java) != null
 
-                    KProperty(
-                            key = key.toString(),
-                            alternateKeys = alternateKeys,
-                            name = name.toString(),
-                            type = type,
-                            nullable = nullable
-                    )
-                }
-                .toList()
+                KProperty(
+                    key = key.toString(),
+                    alternateKeys = alternateKeys,
+                    name = name.toString(),
+                    type = type,
+                    nullable = nullable
+                )
+            }
+            .toList()
 
         val typeAdapterBuilder = TypeSpec
-                .classBuilder(getTypeAdapterClassName(clazz.asClassName()))
-                .addOriginatingElement(clazz)
-                .addAnnotation(getGeneratedAnnotation())
-                .superclass(TypeAdapter::class.parameterizedBy(clazz))
-                .primaryConstructor(
-                        FunSpec.constructorBuilder()
-                                .addParameter(GSON, Gson::class)
-                                .build()
-                )
-                .addProperty(
-                        PropertySpec.builder(GSON, Gson::class)
-                                .initializer(GSON)
-                                .addModifiers(KModifier.PRIVATE)
-                                .build()
-                )
+            .classBuilder(getTypeAdapterClassName(clazz.asClassName()))
+            .addOriginatingElement(clazz)
+            .addAnnotation(getGeneratedAnnotation())
+            .superclass(TypeAdapter::class.parameterizedBy(clazz))
+            .primaryConstructor(
+                FunSpec.constructorBuilder()
+                    .addParameter(GSON, Gson::class)
+                    .build()
+            )
+            .addProperty(
+                PropertySpec.builder(GSON, Gson::class)
+                    .initializer(GSON)
+                    .addModifiers(KModifier.PRIVATE)
+                    .build()
+            )
 
         // init properties
         properties
-                .distinctBy {
-                    it.adapterName
+            .distinctBy {
+                it.adapterName
+            }
+            .forEach {
+                val initializer = CodeBlock.builder()
+                val type = with(ParameterizedTypeName.Companion) {
+                    TypeAdapter::class.asClassName().parameterizedBy(it.type)
                 }
-                .forEach {
-                    val initializer = CodeBlock.builder()
-                    val type = with(ParameterizedTypeName.Companion) {
-                        TypeAdapter::class.asClassName().parameterizedBy(it.type)
-                    }
 
-                    if (it.type is ParameterizedTypeName) {
-                        initializer.add("%L.getAdapter(", GSON)
-                        getParameterizedTypeToken(initializer, it.type)
-                        initializer.add(") as %T", type.javaToKotlinType())
-                    } else {
-                        initializer.add("%L.getAdapter(%T::class.javaObjectType)", GSON, it.type.javaToKotlinType())
-                    }
-
-                    typeAdapterBuilder.addProperty(
-                            PropertySpec.builder(it.adapterName, type.javaToKotlinType(), KModifier.PRIVATE)
-                                    .delegate("lazy(LazyThreadSafetyMode.NONE) { %L }", initializer.build())
-                                    .build()
-                    )
+                if (it.type is ParameterizedTypeName) {
+                    initializer.add("%L.getAdapter(", GSON)
+                    getParameterizedTypeToken(initializer, it.type)
+                    initializer.add(") as %T", type.javaToKotlinType())
+                } else {
+                    initializer.add("%L.getAdapter(%T::class.javaObjectType)", GSON, it.type.javaToKotlinType())
                 }
+
+                typeAdapterBuilder.addProperty(
+                    PropertySpec.builder(it.adapterName, type.javaToKotlinType(), KModifier.PRIVATE)
+                        .delegate("lazy(LazyThreadSafetyMode.NONE) { %L }", initializer.build())
+                        .build()
+                )
+            }
 
         // add write() function
         typeAdapterBuilder.addFunction(generateWriteFunction(clazz, properties))
@@ -137,9 +137,9 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
      */
     private fun generateWriteFunction(clazz: TypeElement, properties: List<KProperty>): FunSpec {
         val writeFunc = FunSpec.builder("write")
-                .addModifiers(KModifier.OVERRIDE)
-                .addParameter(WRITER, JsonWriter::class)
-                .addParameter(OBJECT, clazz.asClassName().asNullable())
+            .addModifiers(KModifier.OVERRIDE)
+            .addParameter(WRITER, JsonWriter::class)
+            .addParameter(OBJECT, clazz.asClassName().asNullable())
 
         //region if
         writeFunc.beginControlFlow("if (%L == null)", OBJECT)
@@ -163,18 +163,25 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
      */
     private fun generateReadFunction(clazz: TypeElement, properties: List<KProperty>): FunSpec {
         val readFunc = FunSpec.builder("read")
-                .addModifiers(KModifier.OVERRIDE)
-                .addParameter(READER, JsonReader::class.java)
-                .returns(clazz.asClassName().asNullable())
+            .addModifiers(KModifier.OVERRIDE)
+            .addParameter(READER, JsonReader::class.java)
+            .returns(clazz.asClassName().asNullable())
 
         readFunc.beginControlFlow("if (%L.peek() == %T.NULL)", READER, JsonToken::class)
         readFunc.addStatement("%L.nextNull()", READER)
         readFunc.addStatement("return null")
         readFunc.endControlFlow()
 
+        val defaultValueProvider = clazz.enclosedElements
+            .find { it.simpleName.contentEquals("Companion") && it.kind == ElementKind.CLASS }
+            ?.enclosedElements
+            ?.find { it.getAnnotation(Default::class.java) != null }
+
         properties.forEach {
             readFunc.addStatement("var ${it.key}: %L = null", it.type.javaToKotlinType().asNullable())
-            readFunc.addStatement("var ${it.key}Assigned = false")
+            if (defaultValueProvider != null) {
+                readFunc.addStatement("var ${it.key}Assigned = false")
+            }
         }
 
         readFunc.addStatement("%L.beginObject()", READER)
@@ -196,7 +203,9 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
             val keysFormat = allKeys.joinToString(separator = ", ") { "%S" }
             readFunc.addStatement("$keysFormat -> {", *allKeys.toTypedArray())
             readFunc.addStatement("  %L = %L.read(%L)", it.key, it.adapterName, READER)
-            readFunc.addStatement("  %L = true", it.key + "Assigned")
+            if (defaultValueProvider != null) {
+                readFunc.addStatement("  %L = true", it.key + "Assigned")
+            }
             readFunc.addStatement("}")
         }
         readFunc.addStatement("else -> %L.skipValue()", READER)
@@ -208,23 +217,18 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
 
         readFunc.addStatement("%L.endObject()", READER)
 
-        val defaultValueProvider = clazz.enclosedElements
-                .find { it.simpleName.contentEquals("Companion") && it.kind == ElementKind.CLASS }
-                ?.enclosedElements
-                ?.find { it.getAnnotation(Default::class.java) != null }
-
         if (defaultValueProvider != null) {
             readFunc.addStatement("val default${clazz.simpleName} = ${clazz.simpleName}.${defaultValueProvider.simpleName}()")
             readFunc.addStatement("return %T(", clazz.asType())
             properties.forEach { field ->
                 readFunc.addStatement(
-                        "%L = if (%L) %L%L else %L.%L,",
-                        field.name,
-                        field.key + "Assigned",
-                        field.key,
-                        if (field.nullable) "" else "!!",
-                        "default" + clazz.simpleName,
-                        field.key,
+                    "%L = if (%L) %L%L else %L.%L,",
+                    field.name,
+                    field.key + "Assigned",
+                    field.key,
+                    if (field.nullable) "" else "!!",
+                    "default" + clazz.simpleName,
+                    field.key,
                 )
             }
             readFunc.addStatement(")")
@@ -232,10 +236,10 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
             readFunc.addStatement("return %T(", clazz.asType())
             properties.forEach { field ->
                 readFunc.addStatement(
-                        "%L = %L%L,",
-                        field.name,
-                        field.key,
-                        if (field.nullable) "" else "!!",
+                    "%L = %L%L,",
+                    field.name,
+                    field.key,
+                    if (field.nullable) "" else "!!",
                 )
             }
             readFunc.addStatement(")")
@@ -244,7 +248,11 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
         return readFunc.build()
     }
 
-    private fun getParameterizedTypeToken(codeBlock: CodeBlock.Builder, type: ParameterizedTypeName, asType: Boolean = false) {
+    private fun getParameterizedTypeToken(
+        codeBlock: CodeBlock.Builder,
+        type: ParameterizedTypeName,
+        asType: Boolean = false,
+    ) {
         codeBlock.add("%T.getParameterized(%T::class.javaObjectType", TypeToken::class, type.rawType.javaToKotlinType())
 
         type.typeArguments.forEach {
@@ -266,10 +274,11 @@ class KsonTypeAdapterProcessor : KsonProcessor() {
     private fun TypeName.javaToKotlinType(): TypeName {
         return if (this is ParameterizedTypeName) {
             (rawType.javaToKotlinType() as ClassName).parameterizedBy(
-                    *typeArguments.map { it.javaToKotlinType() }.toTypedArray()
+                *typeArguments.map { it.javaToKotlinType() }.toTypedArray()
             )
         } else {
-            val className = JavaToKotlinClassMap.INSTANCE.mapJavaToKotlin(FqName(toString()))?.asSingleFqName()?.asString()
+            val className =
+                JavaToKotlinClassMap.INSTANCE.mapJavaToKotlin(FqName(toString()))?.asSingleFqName()?.asString()
 
             return if (className == null) {
                 this
